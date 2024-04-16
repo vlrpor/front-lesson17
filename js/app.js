b913d69bf74df3f4c98bb6fbe339debb74842b7b
vlrpor <v.forchkhidze@gmail.com>
2024-04-16 04:08:02 -0700

    add app and countries js files

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const img = document.createElement('img');
+img.src = 'https://picsum.photos/id/1/200/300';
+
+document.body.appendChild(img);
---
+++ js/countries.js
@@ -0,0 +1,51 @@
+
+const countries = [
+	{
+		name: "Georgia",
+		currencies: {
+			GEL: {
+				name: "lari",
+				symbol: "₾",
+			},
+		},
+		capital: ["Tbilisi"],
+		flag: "https://flagcdn.com/w320/ge.png",
+		map: "https://goo.gl/maps/bvCaGBePR1ZEDK5cA",
+	},
+	{
+		name: "Federal Republic of Germany",
+		currencies: {
+			EUR: {
+				name: "Euro",
+				symbol: "€",
+			},
+		},
+		capital: ["Berlin"],
+		flag: "https://flagcdn.com/w320/de.png",
+		map: "https://goo.gl/maps/mD9FBMq1nvXUBrkv6",
+	},
+	{
+		name: "Ukraine",
+		currencies: {
+			UAH: {
+				name: "Ukrainian hryvnia",
+				symbol: "₴",
+			},
+		},
+		capital: ["Kyiv"],
+		flag: "https://flagcdn.com/w320/ua.png",
+		map: "https://goo.gl/maps/DvgJMiPJ7aozKFZv7",
+	},
+	{
+		name: "United Kingdom of Great Britain and Northern Ireland",
+		currencies: {
+			GBP: {
+				name: "British pound",
+				symbol: "£",
+			},
+		},
+		capital: ["London"],
+		flag: "https://flagcdn.com/w320/gb.png",
+		map: "https://goo.gl/maps/FoDtc3UKMkFsXAjHA",
+	},
+];
